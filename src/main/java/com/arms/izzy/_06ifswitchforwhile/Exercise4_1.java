c552d314c6da28c927a8b69513ffacd0bddb2b82
izzy215 <jihyeon3@gmail.com>
2023-07-25 23:09:49 +0900

    feat: [JAVA-23] #04연습문제 - 23.07.25 #close #time 1h +review SR @313devops
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::izzy ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

---
+++ src/main/java/com/arms/izzy/_06ifswitchforwhile/Exercise4_1.java
@@ -0,0 +1,28 @@
+package com.arms.izzy._06ifswitchforwhile;
+
+public class Exercise4_1 {
+    public static void main(String[] args) {
+        //int형 변수 x가 10보다 크고 20보다 작을때 true
+       int x = 1;
+        if(x  >10 && x<20);
+
+        char ch = 'A';
+        if(ch != ' ' && ch != '\t');
+
+        if(ch =='x'||ch =='X');
+
+        if(ch >'0'&&ch<'9');
+
+        if((ch>'a'&&ch<'z')||(ch>'A'&&ch<'Z'));
+
+        int year = 1;
+        if(year%400==0||(year%4==0&&year%100!=0));
+
+        boolean powerOn = false;
+        if(!powerOn);
+
+        String str = "yes";
+        if(str.equals("yes"));
+
+    }
+}
